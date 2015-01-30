05dbe693abda36b919d62a7a398f42b6b7974df9
Joshua Songy <joshua.songy@gmail.com>
2015-01-30 10:36:42 -0500

    Added tool that writes out the max temperature from kernel sensors. In addition, on msi laptops, this controls the fan.

---
+++ msi/temperature_watchdog/include/fan.h
@@ -0,0 +1,8 @@
+#ifndef FAN_H_
+#define FAN_H_
+
+#include <stdbool.h>
+
+void set_fan_auto(bool state);
+
+#endif /* FAN_H_ */
---
+++ msi/temperature_watchdog/src/fan.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "fan.h"
+
+// The file used for setting the fan state.
+const char* const fan_file =
+"/sys/devices/platform/msi-laptop-pf/auto_fan";
+
+void set_fan_auto(bool state) {
+	// Set the fan's state.
+	FILE* file = fopen("/sys/devices/platform/msi-laptop-pf/auto_fan", "wb");
+	fprintf(file, "%i\n", state);
+	fclose(file);
+}
