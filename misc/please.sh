e26ea5c94c0f8007381edcf9dc20b2af633b7b21
Jay Kamat <jgkamat@users.noreply.github.com>
2015-01-29 21:40:47 -0500

    Explained how to use it!

--- misc/please.sh
+++ misc/please.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# works under arch. Im not sure which dependencies you need, if you need any
+# source this file in your bashrc for great results
+
+# works under arch. Im not sure which dependencies you need, if you need any (besides sudo obviously)
 
 # plz can i has stdio
 alias please='echo "ok: `fc -ln -1`" && sudo `fc -ln -1`'
