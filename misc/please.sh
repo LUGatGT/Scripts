8628d8efc00dd41cca7d4b923a7a5342f9d821e7
jgkamat <github@jgkamat.33mail.com>
2015-02-10 12:00:31 -0500

    Made aliasing work under the please script, (because pretty ls colors)

--- misc/please.sh
+++ misc/please.sh
@@ -1,11 +1,15 @@
-#!/bin/bash
 
-# source this file in your bashrc for great results
+# source this file in your zshrc for great results
 
-# works under arch. Im not sure which dependencies you need, if you need any (besides sudo obviously)
+# works under arch, under zsh. Im not sure which dependencies you need, if you need any (besides sudo obviously)
 
 # plz can i has stdio
-alias please='echo "ok: `fc -ln -1`" && sudo `fc -ln -1`'
+alias sudo='sudo '
+alias please='print -s "sudo $(fc -ln -1)" && echo "ok: $(fc -ln -1)" && eval sudo $(fc -ln -1)'
+alias plz='please '
+zshaddhistory() {
+    [[ ! (("$1" == *please*) || ("$1" == *plz*)) ]]
+}
 
 # Here is a quiter alternative if people prefer, this at least works under zsh. - MiningMarsh
 # alias please='sudo $(fc -ln -1)'
