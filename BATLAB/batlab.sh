85bcb42c6323c743d2da66357be64d3ee9bf0eff
Michael Eden <themichaeleden@gmail.com>
2015-03-28 17:03:08 -0400

    Multiplying and Printing

--- BATLAB/batlab.sh
+++ BATLAB/batlab.sh
@@ -19,51 +19,37 @@ function toMatrix() {
     done < <(echo "$1")
 }
 
-function traverseRow() {
-    SEQ=""
-    ROWS=$( echo "$1" | wc -l )
-    COLS=$( echo "$1" | head -1 | wc -w )
-    while read -r ROW; do
-        for E in $ROW; do
-            SEQ="$SEQ $E"
-        done
-    done < <(echo "$1")
-    echo "${ROWS} ${COLS}${SEQ}"
-}
-
-function traverseCol() {
-    SEQ=""
-    MATRIX="$1"
-    ROWS=$( echo "$MATRIX" | wc -l )
-    COLS=$( echo "$MATRIX" | head -1 | wc -w )
-
-    for COL in {1..$COLS}; do
-        for ROW in {1..$ROWS}; do
-            NEXT=$(getElm "$MATRIX" $ROW $COL)
-            SEQ="$SEQ $NEXT"
-        done
-    done
-
-
-}
-
-
 function multiply() {
-    aMatrix="$1"
-    aRows=$( echo "$aMatrix" | wc -l )
-    aCols=$( echo "$aMatrix" | head -1 | wc -w )
-    bMatrix="$2"
-    bRows=$( echo "$bMatrix" | wc -l )
-    bRows=$( echo "$bMatrix" | head -1 | wc -w )
+    eval "$3=( $ROWS $ROWS )"
+    eval local ROWS='${'${1}'[0]}'
+    eval local COLS='${'${1}'[1]}'
+    local SIZE=$(( $COLS * $ROWS ))
+    local INITIAL=2
 
-    aRow=1
-    aCol=1
-    bRow=1
-    bCol=1
+    for (( i = 0; i < $(( $SIZE * 3 )); i++ )); do
+        # Update RowWise index
+        COLWISE=$(( ( $i % $COLS ) + ( $i / $SIZE ) * $COLS + $INITIAL ))
+        # Update ColWise index
+        ROWWISE=$(( ( ( $i * $ROWS ) % $SIZE ) + ( ( $i / $COLS ) % $ROWS ) + $INITIAL ))
+        # Update of resulting product
+        PRODUCT=$(( i / $COLS + $INITIAL ))
+        # Multiply together!
+        eval ${3}'[$PRODUCT]=$(( ${'${3}'[$PRODUCT]} + ${'${1}'[$ROWWISE]} * ${'${2}'[$COLWISE]} ))'
+    done
+}
 
-    while [ $aRow -le $aRows ]; do
-        while [ $aCol -le $aCols ]; do
+function printMatrix() {
+    eval local ROWS='${'${1}'[0]}'
+    eval local COLS='${'${1}'[1]}'
+    local SIZE=$(( $COLS * $ROWS + 2 ))
 
+    for (( i = 2; i < $SIZE; i++ )); do
+        eval echo -e -n '${'${1}'['${i}']} \\t'
+        # Newlines are different rows
+        if [ $(( ( $i - 2 ) % $COLS )) -eq $(( $COLS - 1 )) ]; then
+            echo
+        fi
+    done
 }
 
 MATRIXA="1 0 0
@@ -77,37 +63,7 @@ MATRIXB="1 0 0
 toMatrix "$MATRIXA" aMatrix
 toMatrix "$MATRIXB" bMatrix
 
-function multiply() {
-    INITIAL=2
-    ROWS=${aMatrix[0]}
-    COLS=${aMatrix[1]}
-    SIZE=$(( $COLS * $ROWS ))
-    RESULT=( $ROWS $ROWS )
+multiply aMatrix bMatrix result
 
-    for (( i = 0; i < $(( $SIZE * 3 )); i++ )); do
-        # Update RowWise index
-        COLWISE=$(( ( $i % $COLS ) + ( $i / $SIZE ) * $COLS + $INITIAL ))
-        # Update ColWise index
-        ROWWISE=$(( ( ( $i * $ROWS ) % $SIZE ) + ( ( $i / $COLS ) % $ROWS ) + $INITIAL ))
-        # Update of resulting product
-        PRODUCT=$(( i / $COLS + $INITIAL ))
-        # Do something with the index
-        RESULT[$PRODUCT]=$(( ${RESULT[$PRODUCT]} + ${aMatrix[$ROWWISE]} * ${bMatrix[$COLWISE]} ))
-        echo -e "${aMatrix[$ROWWISE]} * ${bMatrix[$COLWISE]} = $(( ${aMatrix[$ROWWISE]} * ${bMatrix[$COLWISE]} ))"
-    done
-}
+printMatrix result
 
-
-
-    # # Update the index of col wise
-    # COLWISE=$(( $COLWISE + $COLS ))
-    # if [ $COLWISE -ge $SIZE ]; then
-    #     OFFSET=$(( $OFFSET + 1 ))
-    #     if [ $OFFSET -eq $(( $COLS + 2 )) ]; then
-    #         OFFSET=2
-    #     fi
-    #     COLWISE=$OFFSET
-    # fi
-    # # Update the index of row wise
-    # ROWWISE=$(( $ROWWISE + 1 ))
-    # if [ $ROWWISE -]
